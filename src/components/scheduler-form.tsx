5678352d9dbaa7d603c78f0b23c176500865f391
OakDev <ponce@withoak.com>
2024-02-28 10:59:53 -0600

    Added Event Handlers and States

--- src/components/scheduler-form.tsx
+++ src/components/scheduler-form.tsx
@@ -1,111 +1,130 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SchedulerForm = () => 
-    <header className="as-form">
-        <form>
-            <div className="input-group">
-                <label htmlFor='first-name'> First Name </label>
-                <input type="text" name="first-name" id="first-name" autoComplete='on' />
-            </div>
+function SchedulerForm() {
+    const [inputField , setInputField] = useState({
+        firstName: '',
+        lastName: '',
+        streetAddress: '',
+        addressLine2: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        email: '',
+        phone: '',
+        interest: '',
+        referral: '',
+        message: '',
+        newsletter: ''
+    })
 
-            <div className="input-group">
-                <label htmlFor='last-name'> Last Name </label>
-                <input type="text" name="last-name" id="last-name" />
-            </div>
+    function inputHandler( e: any ) {
+        setInputField( { ...inputField, [e.target.name]: e.target.value } )
+    }
 
-            <div className="input-group">
-                <label htmlFor='street-address'> Street Address </label>
-                <input type="text" name="street-address" id="street-address"  autoComplete='on'/>
-            </div>
+    const handleSubmit = ( e:any ) => {
+        console.log(inputField)
+        e.preventDefault()
+    }
 
-            <div className="input-group">
-                <label htmlFor='address-line-2'> Address Line 2 </label>
-                <input type="text" name="address-line-2" id="address-line-2" />
-            </div>
+    return(
+        <div className="as-form">
+            <span className="required-note">All fields are required unless marked optional</span>
 
-            <div className="input-group">
-                <label htmlFor='city'> City </label>
-                <input type="text" name="city" id="city" />
-            </div>
-
-            <div className="input-group size-short">
-                <label htmlFor='state'> State </label>
-                <input type="text" name="state"  id="state" />
-            </div>
-
-            <div className="input-group size-short">
-                <label htmlFor='zip-code'> Zip Code </label>
-                <input type="text" name="zip-code" id="zip-code" />
-            </div>
-
-            <div className="input-group">
-                <label htmlFor='email'> Email </label>
-                <input type="email" name="email" id="email" autoComplete='on' />
-            </div>
-
-            <div className="input-group">
-                <label htmlFor='phone'> Phone Number </label>
-                <input name="phone" id="phone" type="tel" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"  autoComplete='on' />
-            </div>
-
-            <div className="input-group">
-                <label> What are you interested in? </label>
-
-                <div className="check-option">
-                    <input type="radio" name="interest" value="new-pool-spa" id="new-pool-spa" />
-                    <label htmlFor='new-pool-spa'> Build a New Pool and/or Spa </label>
-                </div>
-                
-                <div className="check-option">
-                    <input type="radio" name="interest" value="renovate-pool-spa" id="renovate-pool-spa"/>
-                    <label htmlFor='renovate-pool-spa'> Renovate an Existing Pool or Spa </label>
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label htmlFor='first-name'> First Name </label>
+                    <input type="text" name="firstName" id="first-name" autoComplete='on' onChange={inputHandler} value={inputField.firstName}/>
                 </div>
 
-                <div className="check-option">
-                    <input type="radio" name="interest" value="backyard-offerings" id="backyard-offerings" />
-                    <label htmlFor='backyard-offerings'> Backyard Offerings </label>
+                <div className="input-group">
+                    <label htmlFor='last-name'> Last Name </label>
+                    <input type="text" name="lastName" id="last-name" onChange={inputHandler} value={inputField.lastName} />
                 </div>
-            </div>
 
-            <div className="input-group">
-                <label> Where you referred by Anthony & Sylvan Customer? </label>
-
-                <div className="check-option">
-                    <input type="radio" name="referral" value="referral-yes" id="referral-yes" />
-                    <label htmlFor='referral-yes'> Yes </label>
+                <div className="input-group">
+                    <label htmlFor='street-address'> Street Address </label>
+                    <input type="text" name="streetAddress" id="street-address" autoComplete='on' onChange={inputHandler} value={inputField.streetAddress} />
                 </div>
-                
-                <div className="check-option">
-                    <input type="radio" name="referral" value="referral-no" id="referral-no" />
-                    <label htmlFor='referral-no'> No </label>
+
+                <div className="input-group">
+                    <label htmlFor='address-line-2'> Address Line 2 </label>
+                    <input type="text" name="addressLine2" id="address-line-2" onChange={inputHandler} value={inputField.addressLine2}  />
                 </div>
-            </div>
 
-            <div className="input-group">
-                <label htmlFor='appointement-date'> Select Appointment Date </label>
-                <input type="date" name="appointement-date" id="appointement-date" />
-            </div>
-
-            <div className="input-group">
-                <label htmlFor='appointement-time'> Select Appointment Time </label>
-                <input type="time" name="appointement-time" id="appointement-time" />
-            </div>
-
-            <div className="input-group size-full">
-                <label htmlFor='message'> Is there anything else we should know to best meet your needs? </label>
-                <textarea name="message" id="message" rows={10} />
-            </div>
-
-            <div className="input-group size-full">
-                <div className="check-option">
-                    <input type="checkbox" name="newsletter" value="newsletter-yes" id="newsletter" />
-                    <label htmlFor='newsletter'> Yes, I would like to receive Anthony & Sylvan's newsletter, special offers and promotions. </label>
+                <div className="input-group">
+                    <label htmlFor='city'> City </label>
+                    <input type="text" name="city" id="city" onChange={inputHandler} value={inputField.city} />
                 </div>
-            </div>
 
-            
-            <input type="submit" value="Submit" />
-        </form>
-    </header>
+                <div className="input-group size-short">
+                    <label htmlFor='state'> State </label>
+                    <input type="text" name="state"  id="state" onChange={inputHandler} value={inputField.state} />
+                </div>
+
+                <div className="input-group size-short">
+                    <label htmlFor='zip-code'> Zip Code </label>
+                    <input type="text" name="zipCode" id="zip-code" onChange={inputHandler} value={inputField.zipCode} />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor='email'> Email </label>
+                    <input type="email" name="email" id="email" autoComplete='on' onChange={inputHandler} value={inputField.email} />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor='phone'> Phone Number </label>
+                    <input name="phone" id="phone" type="tel" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"  autoComplete='on' onChange={inputHandler} value={inputField.phone} />
+                </div>
+
+                <div className="input-group">
+                    <label> What are you interested in? </label>
+
+                    <div className="check-option">
+                        <input type="radio" name="interest" value="new-pool-spa" id="new-pool-spa" onChange={inputHandler} />
+                        <label htmlFor='new-pool-spa'> Build a New Pool and/or Spa </label>
+                    </div>
+                    
+                    <div className="check-option">
+                        <input type="radio" name="interest" value="renovate-pool-spa" id="renovate-pool-spa" onChange={inputHandler} />
+                        <label htmlFor='renovate-pool-spa'> Renovate an Existing Pool or Spa </label>
+                    </div>
+
+                    <div className="check-option">
+                        <input type="radio" name="interest" value="backyard-offerings" id="backyard-offerings" onChange={inputHandler} />
+                        <label htmlFor='backyard-offerings'> Backyard Offerings </label>
+                    </div>
+                </div>
+
+                <div className="input-group">
+                    <label> Where you referred by Anthony & Sylvan Customer? </label>
+
+                    <div className="check-option">
+                        <input type="radio" name="referral" value="yes" id="referral-yes" onChange={inputHandler} />
+                        <label htmlFor='referral-yes'> Yes </label>
+                    </div>
+                    
+                    <div className="check-option">
+                        <input type="radio" name="referral" value="no" id="referral-no" onChange={inputHandler} />
+                        <label htmlFor='referral-no'> No </label>
+                    </div>
+                </div>
+
+                <div className="input-group size-full">
+                    <label htmlFor='message'> Is there anything else we should know to best meet your needs? </label>
+                    <textarea name="message" id="message" rows={10} onChange={inputHandler} value={inputField.message} />
+                </div>
+
+                <div className="input-group size-full">
+                    <div className="check-option">
+                        <input type="checkbox" name="newsletter" value="yes" id="newsletter" onChange={inputHandler} />
+                        <label htmlFor='newsletter'> Yes, I would like to receive Anthony & Sylvan's newsletter, special offers and promotions. </label>
+                    </div>
+                </div>
+
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
 
 export default SchedulerForm
