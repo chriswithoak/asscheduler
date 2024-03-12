ddc23021a69b3d11e262377a833c4c91060bba7c
OakDev <ponce@withoak.com>
2024-03-11 21:59:12 -0500

    APP: Changed Success Message

--- src/components/confirmation.tsx
+++ src/components/confirmation.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Confirmation = () => 
     <div className="confirmation">
         <h2>Success!</h2>
-        <p> Our experts will be in touch shortly to confirm your appointment.</p>
+        <p> Please select your preferred day and time for a consultation.</p>
 
         <img src="/calendar.png" className='calendar' alt="calendar"/>
     </div>
