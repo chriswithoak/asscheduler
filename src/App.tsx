3e483994a4f4ae437012f8ac467468cf4d35d8dd
OakDev <ponce@withoak.com>
2024-03-13 18:03:16 -0500

    APP: Transferred Message Event Listener to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect  } from 'react';
 import './css/global.css';
 import Header from './components/header';
 import SchedulerForm from './components/scheduler-form';
 
 function App() {
+
+  const handleMessageEvent = ( event:any ) => {
+    if (event.origin !== "https://oakdevsandbox.wpenginepowered.com/") return;
+
+    sessionStorage.setItem("utm_campaign", event.data.utm_campaign);
+    sessionStorage.setItem("utm_medium", event.data.utm_medium);
+    sessionStorage.setItem("utm_source", event.data.utm_source);
+    sessionStorage.setItem("AdSrc", event.data.AdSrc);
+  }
+
+  useEffect(() => {
+    window.addEventListener("onmessage", handleMessageEvent);
+  }, []);
+
   return (
     <div className="as-scheduler">
       <Header />
