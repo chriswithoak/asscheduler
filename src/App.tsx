4f45cfe55f80dff026770983fb361284b864e739
OakDev <ponce@withoak.com>
2024-03-13 18:53:43 -0500

    APP: Removed non working event on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React, {useEffect  } from 'react';
+import React from 'react';
 import './css/global.css';
 import Header from './components/header';
 import SchedulerForm from './components/scheduler-form';
 
 function App() {
-  useEffect(() => {
-    window.addEventListener("message", ( event ) => {
-      console.log("HANDLED ON APP");
-      console.log(event);
-      if (event.origin !== "http://localhost:3000" && event.origin !== "https://oakdevsandbox.wpenginepowered.com") return;
-
-      sessionStorage.setItem("utm_campaign", event.data.utm_campaign);
-      sessionStorage.setItem("utm_medium", event.data.utm_medium);
-      sessionStorage.setItem("utm_source", event.data.utm_source);
-      sessionStorage.setItem("AdSrc", event.data.AdSrc);
-      console.log("SAVED ON APPP");
-
-    });
-  }, []);
-
   return (
     <div className="as-scheduler">
       <Header />
